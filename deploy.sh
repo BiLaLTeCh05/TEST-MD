3311e188a94b6a3f70d9bad5c14fa04795301434
BILAL TECH <penalacc786@gmail.com>
2025-09-07 07:03:33 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Private repository ka URL aur token
-REPO_URL="https:                                            
+REPO_URL="https://github.com/BilalTech05/BILAL-MD.git
 TOKEN="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"
 
 # Plugins aur lib folders ko download karne ke liye
-git clone https://github.com/BilalTech05/BILAL-MD.git temp-repo
+git clone https://github.com/BilalTech05/TEST-MD.git temp-repo
 cp -r temp-repo/plugins .
 cp -r temp-repo/lib .
 rm -rf temp-repo
