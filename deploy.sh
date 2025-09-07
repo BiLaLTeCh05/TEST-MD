077081b0f7916ac603304422b2d8abb759235142
BILAL TECH <penalacc786@gmail.com>
2025-09-07 23:39:02 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 TOKEN="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"
 
 # Plugins aur lib folders ko download karne ke liye
-git clone --depth 1 https://${TOKEN}@github.com/BilalTech05/BILAL-MD.git
+git clone --depth 1 https://github.com/BilalTech05/BILAL-MD.git
 cp -r BILAL-MD/plugins BILAL-MD/lib .
 rm -rf BILAL-MD
 
@@ -14,7 +14,7 @@ pm2 start index.js --deep-monitoring --attach --name BILAL-MD
 
 # Changes ko private repository mein push karne ke liye
 git init
-git remote add origin https://${TOKEN}@github.com/BilalTech05/BILAL-MD.git
+git remote add origin https://github.com/BilalTech05/BILAL-MD.git
 git config --global user.email "jomeh40972@certve.com"
 git config --global user.name "BILAL"
 git add .
